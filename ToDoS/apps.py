40edc2628446dbdd440834323f6d2ac7fb8a026a
MrMihen13 <kmerezhnikov2002@yandex.ru>
2021-09-06 10:08:02 +0300

    Start app 'ToDoS'

--- ToDoS/apps.py
+++ ToDoS/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TestapponeConfig(AppConfig):
+class TodosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testappone'
+    name = 'ToDoS'
