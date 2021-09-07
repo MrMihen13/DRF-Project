9479b983483a56504c9e8def4e7ef869a0adbd99
MrMihen13 <kmerezhnikov2002@yandex.ru>
2021-09-07 12:44:41 +0300

    Improving the login/logout modul

--- FirstProgect/urls.py
+++ FirstProgect/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/base-auth/', include('rest_framework.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth_token/', include('djoser.urls.authtoken')),
     path('api/v1/cars/', include('cars.urls')),
     path('api/v1/todos/', include('ToDoS.urls')),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,24 +1,27 @@
 from rest_framework import generics
+# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
 
 from cars.serializers import CarDetailSerializer, CarListSerializer
 from cars.models import Car
-from cars.permission import IsOwnerOrReadOnly
-from rest_framework.permissions import IsAuthenticated
+# from cars.permission import IsOwnerOrReadOnly
 
 
 class CarCreateView(generics.CreateAPIView):
     serializer_class = CarDetailSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class CarsListView(generics.ListAPIView):
     serializer_class = CarListSerializer
     queryset = Car.objects.all()
-    permission_classes = (IsAuthenticated,)
 
 
 class CarDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CarDetailSerializer
     queryset = Car.objects.all()
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAdminUser, IsAuthenticated,)
 
 # Need to make more view
